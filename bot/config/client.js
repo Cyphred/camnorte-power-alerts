32a1c0618b030929a761b9f61a2cd0e35cc7962b
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-04-27 14:21:21 +0800

    Adds client ID and client secret to authentication

--- bot/config/client.js
+++ bot/config/client.js
@@ -2,6 +2,8 @@ const TwitterApi = require("twitter-api-v2").default;
 require("dotenv").config();
 
 const client = new TwitterApi({
+  clientId: process.env.TWITTER_CLIENT_ID,
+  clientSecret: process.env.TWITTER_CLIENT_SECRET,
   appKey: process.env.TWITTER_APP_KEY,
   appSecret: process.env.TWITTER_APP_SECRET,
   accessToken: process.env.TWITTER_ACCESS_TOKEN,
