8a138f6d1d2f28d51a9a5003e19af8f425134576
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-04-28 23:43:30 +0800

    Adds function for translating tagalog months into english

---
+++ bots/twitter/util/tagalogMonthToEnglish.js
@@ -0,0 +1,40 @@
+const tagalogMonthToEnglish = (month) => {
+  const code = month.split(0, 3).toUpperCase();
+
+  switch (code) {
+    case "ENE":
+    case "JAN":
+      return "January";
+    case "PEB":
+    case "FEB":
+      return "February";
+    case "MAR":
+      return "March";
+    case "ABR":
+    case "APR":
+      return "April";
+    case "MAY":
+      return "May";
+    case "HUN":
+    case "JUN":
+      return "June";
+    case "HUL":
+    case "JUL":
+      return "July";
+    case "AGO":
+    case "AUG":
+      return "August";
+    case "SET":
+    case "SEP":
+      return "September";
+    case "OKT":
+    case "OCT":
+      return "October";
+    case "NOB":
+    case "NOV":
+      return "November";
+    case "DIS":
+    case "DEC":
+      return "December";
+  }
+};
