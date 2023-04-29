85f8f1ed4e7552530122c553f8a3efd7313359db
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-04-29 10:10:53 +0800

    Runs scraper on init

--- backend/scrapers/startScrapers.js
+++ backend/scrapers/startScrapers.js
@@ -1,10 +1,14 @@
 const ScrapeScheduledInterruptions = require("./scheduledInterruptionScraper");
 
 const start = () => {
+  // Set intervals for scraping
   setInterval(
     ScrapeScheduledInterruptions,
     process.env.SCHEDULED_INTERRUPTIONS_SCRAPE_INTERVAL
   );
+
+  // Run scrapes on first load
+  ScrapeScheduledInterruptions();
 };
 
 module.exports = start;
